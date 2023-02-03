3ed5847e471956c409ecd98514b0b1341c17c7e3
Hee Seo Kim <hisounni@ewhain.net>
2023-02-03 15:14:10 +0900

    <test>

--- app/src/main/java/org/example/calculator_test/MainActivity.java
+++ app/src/main/java/org/example/calculator_test/MainActivity.java
@@ -3,12 +3,32 @@ package org.example.calculator_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import org.example.calculator_test.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isFirstInput = true;
+    ActivityMainBinding activityMainBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
     }
+
+    public void num1Click(View view){
+        activityMainBinding.resultTextView.append("1");
+
+
+    }
+
+
+
+
 }
+
+
