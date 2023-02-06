8dcd94dad466a2dbdb73f721de4db824c0da81e0
Hee Seo Kim <hisounni@ewhain.net>
2023-02-06 16:55:26 +0900

    <test>

--- app/src/main/java/org/example/calculator_test/MainActivity.java
+++ app/src/main/java/org/example/calculator_test/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.example.calculator_test.databinding.ActivityMainBinding;
 
@@ -13,7 +14,9 @@ import java.lang.invoke.LambdaConversionException;
 public class MainActivity extends AppCompatActivity {
 
     boolean isFirstInput = true;
+    boolean isOperatorClick = false;
     double resultNumber = 0;
+    double inputNumber  = 0;
     String operator = "＋";
 
     ActivityMainBinding activityMainBinding;
@@ -30,36 +33,73 @@ public class MainActivity extends AppCompatActivity {
         String getButtonText = view.getTag().toString();
         if(isFirstInput){
             activityMainBinding.resultTextView.setText(getButtonText);
-            activityMainBinding.mathTextView.setText(getButtonText);
             isFirstInput = false;
+            if(operator.equals("=")){
+                activityMainBinding.mathTextView.setText(null);
+            }
         }else{
-            activityMainBinding.resultTextView.append(getButtonText);
-            activityMainBinding.mathTextView.append(getButtonText);
+            if(activityMainBinding.resultTextView.getText().toString().equals("0")){
+                Toast.makeText(this, "0으로 시작되는 숫자는 없습니다.", Toast.LENGTH_SHORT).show();
+                isFirstInput = true;
+            }else {
+                activityMainBinding.resultTextView.append(getButtonText);
+            }
         }
 
     }
 
+    public void allClearButtonClick (View view){
+        activityMainBinding.resultTextView.setText("0");
+        activityMainBinding.mathTextView.setText("");
+        resultNumber = 0;
+        operator = "＋";
+        isFirstInput = true;
+    }
+
+    public void pointButtonClick (View view){
+        if(isFirstInput) {
+            activityMainBinding.resultTextView.setText("0" + view.getTag().toString());
+            isFirstInput = false;
+        }else {
+            if(activityMainBinding.resultTextView.getText().toString().contains(".")){
+                Toast.makeText(this, "이미 소숫점이 존재합니다.", Toast.LENGTH_SHORT).show();
+            }else {
+                activityMainBinding.resultTextView.append(view.getTag().toString());
+            }
+        }
+    }
     public void operatorClick (View view){
-        double inputNumber = Double.parseDouble(activityMainBinding.resultTextView.getText().toString());
+        isOperatorClick = true;
+        inputNumber = Double.parseDouble(activityMainBinding.resultTextView.getText().toString());
+
+        resultNumber = calculator(resultNumber,inputNumber,operator);
 
         if(operator.equals("＋")){
             resultNumber = resultNumber + inputNumber;
-        }else if(operator.equals("－")){
+        } else if (operator.equals("－")) {
             resultNumber = resultNumber - inputNumber;
-        }else if(operator.equals("×")){
+        } else if (operator.equals("×")) {
             resultNumber = resultNumber * inputNumber;
         } else if (operator.equals("÷")) {
             resultNumber = resultNumber / inputNumber;
         }
-
-        activityMainBinding.resultTextView.setText(String.valueOf(resultNumber));
-        isFirstInput = true;
-        operator = view.getTag().toString();
-
-
+        return resultNumber;
     }
 
+    public void equalsButtonClick (View view){
+        if(isOperatorClick) {
 
+            inputNumber = Double.parseDouble(activityMainBinding.resultTextView.getText().toString());
+
+            resultNumber = calculator(resultNumber, inputNumber, operator);
+
+            activityMainBinding.resultTextView.setText(String.valueOf(resultNumber));
+            isFirstInput = true;
+            operator = view.getTag().toString();
+            activityMainBinding.mathTextView.append(inputNumber + " " + operator + " ");
+        }
+
+    }
 
 
 }
